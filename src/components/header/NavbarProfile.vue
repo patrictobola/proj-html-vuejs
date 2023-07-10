9cc1b8ac273068d8f2e42633f957c4c54e06a82e
patrictobola <tobola92@gmail.com>
2023-07-10 15:44:34 +0200

    imported fontawesome and added icons in header

--- src/components/header/NavbarProfile.vue
+++ src/components/header/NavbarProfile.vue
@@ -3,7 +3,28 @@
 </script>
 
 <template>
-    <h1> asdasd</h1>
+    <nav class="container profile">
+        <div class=" d-flex justify-content-between align-items-center">
+            <div class="icons d-flex">
+                <FontAwesomeIcon icon="fa-brands fa-facebook-f" />
+                <FontAwesomeIcon icon="fa-brands fa-twitter" />
+                <FontAwesomeIcon icon="fa-brands fa-instagram" />
+                <FontAwesomeIcon icon="fa-brands fa-youtube" />
+            </div>
+            <div><span>Sponsors</span></div>
+            <div><span>IMenu</span></div>
+        </div>
+    </nav>
 </template>
 
-<style></style>
+<style lang="scss" scoped>
+@import "../../scss/_variables.scss";
+
+.profile>div {
+    height: 40px;
+}
+
+.profile {
+    border-bottom: 1px solid $light-gray;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,22 @@ import { createApp } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Fontawesome 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faFacebookF} from '@fortawesome/free-brands-svg-icons'
+import { faTwitter} from '@fortawesome/free-brands-svg-icons'
+import { faInstagram} from '@fortawesome/free-brands-svg-icons'
+import { faYoutube} from '@fortawesome/free-brands-svg-icons'
+
+/* add icons to the library */
+library.add(faFacebookF, faTwitter, faYoutube,faInstagram )
+
+createApp(App)
+.component('FontAwesomeIcon', FontAwesomeIcon)
+.mount('#app')
