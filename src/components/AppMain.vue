b6f12683cf9db92883edae6b2260b38c61da8fd2
patrictobola <tobola92@gmail.com>
2023-07-11 13:19:47 +0200

    hardcoded 1 collection card

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,11 +2,13 @@
 import JumbotronPage from './main/JumbotronPage.vue'
 import SectionTitle from './main/SectionTitle.vue'
 import SectionContainer from './main/SectionContainer.vue';
+import SectionCollection from './main/SectionCollection.vue';
 export default {
     components: {
         JumbotronPage,
         SectionTitle,
         SectionContainer,
+        SectionCollection,
     }
 }
 </script>
@@ -15,6 +17,7 @@ export default {
     <JumbotronPage />
     <SectionTitle :title="`Featured Products`" :description="`Must have products from our top sellers`" />
     <SectionContainer />
+    <SectionCollection />
 </template>
 
 <style></style>
