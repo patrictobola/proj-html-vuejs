c080a069759873ef620ea346b9a275b09f16a3a1
patrictobola <tobola92@gmail.com>
2023-07-11 10:47:54 +0200

    roughly done buttons in section container

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,10 +1,12 @@
 <script>
 import JumbotronPage from './main/JumbotronPage.vue'
 import SectionTitle from './main/SectionTitle.vue'
+import SectionContainer from './main/SectionContainer.vue';
 export default {
     components: {
         JumbotronPage,
         SectionTitle,
+        SectionContainer,
     }
 }
 </script>
@@ -12,6 +14,7 @@ export default {
 <template>
     <JumbotronPage />
     <SectionTitle :title="`Featured Products`" :description="`Must have products from our top sellers`" />
+    <SectionContainer />
 </template>
 
 <style></style>
---
+++ src/components/main/SectionContainer.vue
@@ -0,0 +1,24 @@
+<script>
+
+</script>
+
+<template>
+    <div class="button-container text-center">
+        <a href="#">Men</a>
+        <a href="#">Women</a>
+        <a href="#">Accessories</a>
+    </div>
+</template>
+
+<style scoped>
+a {
+    font-weight: bold;
+    padding: 10px 30px;
+    background: gainsboro;
+    border: 1px solid gray;
+}
+
+.button-container {
+    border-radius: 3px;
+}
+</style>
