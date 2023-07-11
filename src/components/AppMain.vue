b139b1e71cedf9d21ca9c17efc5c1e4bc0c0cf5d
patrictobola <tobola92@gmail.com>
2023-07-11 10:24:21 +0200

    done reusable sectiontitle

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,14 +1,17 @@
 <script>
 import JumbotronPage from './main/JumbotronPage.vue'
+import SectionTitle from './main/SectionTitle.vue'
 export default {
     components: {
-        JumbotronPage
+        JumbotronPage,
+        SectionTitle,
     }
 }
 </script>
 
 <template>
     <JumbotronPage />
+    <SectionTitle :title="`Featured Products`" :description="`Must have products from our top sellers`" />
 </template>
 
 <style></style>
--- src/components/main/SectionTitle.vue
+++ src/components/main/SectionTitle.vue
@@ -1,7 +1,49 @@
 <script>
-
+export default {
+    props: {
+        title: String,
+        description: String,
+    }
+}
 </script>
 
-<template></template>
+<template>
+    <section class="container">
+        <div class="title-container d-flex">
+            <div class="linebreak"></div>
+            <div class="title align-self-center text-center">
+                <h2>{{ title }}</h2>
+                <span>{{ description }}</span>
+            </div>
+            <div class="linebreak"></div>
+        </div>
+    </section>
+</template>
 
-<style></style>
+<style lang="scss" scoped>
+@import '../../scss/variables';
+
+.title-container {
+    height: 200px;
+}
+
+.linebreak {
+    height: 50%;
+    border-bottom: 1px solid $gray;
+    flex-grow: 1;
+}
+
+.title {
+    margin: 0 40px;
+    padding-top: 20px;
+
+    h2 {
+        font-weight: bold;
+        font-size: 2.5rem;
+    }
+
+    span {
+        color: $gray;
+    }
+}
+</style>
