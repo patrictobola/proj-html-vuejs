90a1fc4a066cc0c610dd75f5ecde658f89fd8209
patrictobola <tobola92@gmail.com>
2023-07-12 16:47:47 +0200

    done footer layout

--- src/components/footer/FooterMain.vue
+++ src/components/footer/FooterMain.vue
@@ -1,7 +1,27 @@
-<script></script>
+<script>
+import ProductTags from './footer_main_components/ProductTags.vue';
+import RecentPosts from './footer_main_components/RecentPosts.vue';
+import TopProducts from './footer_main_components/TopProducts.vue';
+import WebshopInfo from './footer_main_components/WebshopInfo.vue';
+export default {
+    components: {
+        ProductTags,
+        RecentPosts,
+        TopProducts,
+        WebshopInfo,
+    }
+}
+</script>
 <template>
     <section>
-
+        <div class="container h-100">
+            <div class="row">
+                <WebshopInfo />
+                <TopProducts />
+                <RecentPosts />
+                <ProductTags />
+            </div>
+        </div>
     </section>
 </template>
 <style lang="scss" scoped >
---
+++ src/components/footer/footer_main_components/WebshopInfo.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <div class="col-3"></div>
+</template>
+
+<style lang="scss" scoped></style>
