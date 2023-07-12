81227b6bf10b5273aa223011d7dc6f4c6185737a
patrictobola <tobola92@gmail.com>
2023-07-12 16:57:20 +0200

    top rated products in footer done

--- src/components/footer/FooterMain.vue
+++ src/components/footer/FooterMain.vue
@@ -3,21 +3,27 @@ import ProductTags from './footer_main_components/ProductTags.vue';
 import RecentPosts from './footer_main_components/RecentPosts.vue';
 import TopProducts from './footer_main_components/TopProducts.vue';
 import WebshopInfo from './footer_main_components/WebshopInfo.vue';
+import { store } from '../../data/store'
 export default {
     components: {
         ProductTags,
         RecentPosts,
         TopProducts,
         WebshopInfo,
+    },
+    data() {
+        return {
+            store
+        }
     }
 }
 </script>
 <template>
     <section>
         <div class="container h-100">
-            <div class="row">
+            <div class="row h-100 align-items-center">
                 <WebshopInfo />
-                <TopProducts />
+                <TopProducts :webStore="store.webStore.topRated" />
                 <RecentPosts />
                 <ProductTags />
             </div>
