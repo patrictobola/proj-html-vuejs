095babda693b8b642dccf9b8815c8e762d53e716
patrictobola <tobola92@gmail.com>
2023-07-12 19:06:04 +0200

    done card hovers

--- src/components/main/cards/BestsellerCard.vue
+++ src/components/main/cards/BestsellerCard.vue
@@ -1,14 +1,10 @@
 <script>
-import InfoCard from './InfoCard.vue'
 export default {
-    components: {
-        InfoCard,
-    },
     methods: {
         getImagePath(image) {
             const url = new URL(`/src/assets/images/card_models/${image}`, import.meta.url)
             return url.href
-        }
+        },
     },
     props: {
         imagePath: String,
@@ -17,19 +13,41 @@ export default {
         tags: Array,
         name: String,
 
-    }
+    },
 }
 </script>
 
 <template>
     <div class="base-card">
         <img :src="getImagePath(imagePath)" alt="foto">
-        <InfoCard v-if="name" :name="name" :tags="tags" :originalPrice="originalPrice" :finalPrice="finalPrice" />
+        <div class="overlay">
+            <div class="w-100 h-100 d-flex justify-content-between  flex-column text-center pt-5">
+                <div class="mt-5">
+                    <h5>{{ name }}</h5>
+                    <ul class="d-flex justify-content-center">
+                        <li v-for="tag in tags" :key="tag">{{ tag }}</li>
+                    </ul>
+                    <span class="d-flex justify-content-center">${{ finalPrice }}</span>
+                </div>
+                <div class="d-flex justify-content-between align-items-end mb-3 px-3">
+                    <div><a href="#">Add to cart</a></div>
+                    <div><a href="#">Details</a></div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
 @import '../../../scss/variables';
+
+.base-card {
+    position: relative;
+}
+
+ul {
+    height: 50px;
+}
 
 img {
     opacity: 1;
@@ -42,11 +60,12 @@ img {
 .overlay {
     transition: .5s ease;
     opacity: 0;
+    color: white;
     position: absolute;
     top: 0;
     left: 0;
     right: 0;
-    bottom: 100px;
+    bottom: 0px;
     border-radius: 10px;
     background: rgb(255, 130, 221);
     background: linear-gradient(0deg, rgba(255, 130, 221, 1) 0%, rgba(0, 176, 255, 1) 100%);
@@ -54,5 +73,10 @@ img {
 
 .base-card:hover .overlay {
     opacity: 0.9;
+}
+
+a {
+    color: white;
+    font-size: 0.8rem;
 }
 </style>
