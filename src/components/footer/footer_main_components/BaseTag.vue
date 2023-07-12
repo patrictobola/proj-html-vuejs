735411dd5867a406d4361de45c596be18d6b2897
patrictobola <tobola92@gmail.com>
2023-07-12 17:41:23 +0200

    tags section done

---
+++ src/components/footer/footer_main_components/BaseTag.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    props: {
+        name: String
+    }
+}
+</script>
+
+
+<template>
+    <a href="#">{{ name }}</a>
+</template>
+
+
+<style lang="scss" scoped>
+@import '../../../scss/variables';
+
+a {
+    margin: 2px;
+    color: white;
+    padding: 6px 10px;
+    border: 1px solid $dark-gray;
+
+    &:hover {
+        background-color: $blue;
+    }
+}
+</style>
--- src/components/footer/footer_main_components/ProductTags.vue
+++ src/components/footer/footer_main_components/ProductTags.vue
@@ -1,9 +1,29 @@
 <script>
+import BaseTag from './BaseTag.vue';
+import { store } from '../../../data/store';
 export default {
-
+    components: {
+        BaseTag,
+    },
+    data() {
+        return {
+            tags: store.tags
+        }
+    }
 }
 </script>
 
-<template></template>
+<template>
+    <div class="col-3 text-white h-75">
+        <h5>TAGS</h5>
+        <div class="d-flex flex-wrap">
+            <BaseTag v-for="tag in tags" :name="tag.name" />
+        </div>
+    </div>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+h5 {
+    font-weight: 300;
+}
+</style>
